b14a8fc5e57665adb78c3fa808c9ffb82b473033
janebowlinburt <janebowlinburt@gmail.com>
2015-06-12 14:17:52 -0500

    Change the devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/home'
+  root 'pages#home'
 
-  root "pages#home"
-  get "about" => "pages#about" # creates about_path
+  get 'about' => 'pages#about' 
+
+ devise_for :users
+
+    # creates about_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
